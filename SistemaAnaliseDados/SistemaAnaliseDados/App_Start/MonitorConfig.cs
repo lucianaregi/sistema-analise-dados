43948c104f0b3e303ab1cc7e803f070fbdc1d19e
lucianaregi <lucianaregi@gmail.com>
2019-08-26 09:12:27 -0300

    Created monitor config file.

---
+++ SistemaAnaliseDados/SistemaAnaliseDados/App_Start/MonitorConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace SistemaAnaliseDados.App_Start
+{
+    public class MonitorConfig
+    {
+        public static void RegisterWatchers()
+        {
+            var fsw = new FileSystemWatcher
+            {
+                Path = AppDomain.CurrentDomain.BaseDirectory + @"Data\In",
+                EnableRaisingEvents = true,
+                IncludeSubdirectories = false
+            };
+
+            fsw.Created += new FileSystemEventHandler(OnCreated);
+        }
+
+        static void OnCreated(object sender, FileSystemEventArgs e)
+        {
+           
+        }
+    }
+}
--- SistemaAnaliseDados/SistemaAnaliseDados/Global.asax.cs
+++ SistemaAnaliseDados/SistemaAnaliseDados/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SistemaAnaliseDados.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,7 @@ namespace SistemaAnaliseDados
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            MonitorConfig.RegisterWatchers();
         }
     }
 }
